50892d7aad7158875ff6e94fb769199771312736
aaron chen <ixdezbtoeck@gmail.com>
2020-04-25 15:59:09 -0700

    PersonalityQuiz Done

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Aaron Chen on 2020-04-23.
+//  Copyright © 2020 Aaron Chen. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func unwindToQuizIntroduction(segue:
+    UIStoryboardSegue) {
+     
+    }
+}
+
---
+++ PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Aaron Chen on 2020-04-24.
+//  Copyright © 2020 Aaron Chen. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    @IBOutlet var resultAnswerLabel: UILabel!
+    @IBOutlet var resultDefinitionLabel: UILabel!
+    var responses: [Answer]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
+    }
+    
+    func calculatePersonalityResult() {
+        var frequencyOfAnswers: [AnimalType: Int] = [:]
+        let responseTypes = responses.map { $0.type }
+        for response in responseTypes {
+            let newCount: Int
+            if let oldCount = frequencyOfAnswers[response] {
+                newCount = oldCount + 1
+            } else {
+                newCount = 1
+            }
+            frequencyOfAnswers[response] = newCount
+        }
+        let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 > $1.1 }.first!.key
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+    }
+}
