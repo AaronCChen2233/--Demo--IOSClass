5bc816ffc514e8987a6d8d9ff6cb115228df6c19
aaron chen <ixdezbtoeck@gmail.com>
2020-05-22 14:14:45 -0700

    Add Assignment8 project

---
+++ Assignment8/Assignment8/model/RestaurantsCategories.swift
@@ -0,0 +1,22 @@
+//
+//  RestaurantsCategories.swift
+//  Assignment8
+//
+//  Created by Aaron Chen on 2020-05-22.
+//  Copyright © 2020 Aaron Chen. All rights reserved.
+//
+
+import Foundation
+
+enum RestaurantsCategories{
+    case fastFood
+    case italian
+    case japanese
+    case taiwanese
+    case mexican
+    case bar
+    case cafe
+    case breakfast
+    case lunch
+    case dinner
+}
